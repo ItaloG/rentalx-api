5aa31706b8e5bad28b1b2eea85e716ecd4869235
Italo Gabriel <italog03@gmail.com>
2022-05-23 09:33:08 -0300

    feat: add category creation by CategoriesRepo

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,14 +1,14 @@
 import { Router } from 'express'
-
-import { Category } from '../model/Category'
+import { CategoriesRepository } from '../repositories/CategoriesRepository'
 
 const categoriesRoutes = Router()
+const categoriesRepository = new CategoriesRepository()
 
 categoriesRoutes.post("/", (req, res) => {
     const { name, description } = req.body
+    categoriesRepository.create({ name, description })
 
-
-    return res.status(201).json(category)
+    return res.status(201).send()
 })
 
 export { categoriesRoutes }
