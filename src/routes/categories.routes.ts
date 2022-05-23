89643ec8b86cf50fe9dc322d554c6936de176eae
Italo Gabriel <italog03@gmail.com>
2022-05-23 09:26:56 -0300

    chore: change eslint configs

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -4,15 +4,9 @@ import { Category } from '../model/Category'
 
 const categoriesRoutes = Router()
 
-const categories: Category[] = []
-
 categoriesRoutes.post("/", (req, res) => {
     const { name, description } = req.body
 
-    const category = new Category()
-    Object.assign(category, {name, description, created_at: new Date()})
-
-    categories.push(category)
 
     return res.status(201).json(category)
 })
