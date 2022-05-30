12e88e7cdb504a7d4cd9a1a6deefa064108017df
Italo Gabriel <italog03@gmail.com>
2022-05-30 11:45:53 -0300

    chore: add migrations path

--- src/database/index.ts
+++ src/database/index.ts
@@ -2,11 +2,14 @@ import { DataSource } from 'typeorm'
 
 const dataSource = new DataSource({
     type: 'postgres',
-    host: 'database_ignite',
+    host: 'localhost',
     port: 5432,
     username: "docker",
     password: "ignite",
-    database: "rentx"
+    database: "rentx",
+    migrations: ["src/database/migrations/*.ts"]
 })
 
 dataSource.initialize()
+
+export { dataSource } 
