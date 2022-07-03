47613d48ea3e86b29ad54dc33c5fee395fe4b629
Italo Gabriel <italog03@gmail.com>
2022-07-03 11:39:34 -0300

    feat: add UpdateUserAddAvatar migration

--- src/modules/accounts/entities/User.ts
+++ src/modules/accounts/entities/User.ts
@@ -1,26 +1,35 @@
-import { v4 as uuidV4 } from 'uuid'
-import { Column, CreateDateColumn, Entity, PrimaryColumn } from 'typeorm'
+import { v4 as uuidV4 } from "uuid";
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm";
 
 @Entity("users")
 class User {
-    @PrimaryColumn()
-    id?: string
-    @Column()
-    name: string
-    @Column()
-    password: string
-    @Column()
-    email: string
-    @Column()
-    driver_license: string
-    @Column()
-    isAdmin: string
-    @CreateDateColumn()
-    created_at: Date
+  @PrimaryColumn()
+  id?: string;
+  
+  @Column()
+  name: string;
+  
+  @Column()
+  password: string;
+  
+  @Column()
+  email: string;
+  
+  @Column()
+  driver_license: string;
+  
+  @Column()
+  isAdmin: string;
+  
+  @Column()
+  avatar: string;
 
-    constructor() {
-        if(!this.id) this.id = uuidV4()
-    }
+  @CreateDateColumn()
+  created_at: Date;
+
+  constructor() {
+    if (!this.id) this.id = uuidV4();
+  }
 }
 
-export { User }
+export { User };
