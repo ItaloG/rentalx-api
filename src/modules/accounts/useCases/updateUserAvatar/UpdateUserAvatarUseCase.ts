06dba8957428180d088639a57f2849a4d5536963
Italo Gabriel <italog03@gmail.com>
2022-07-09 13:17:47 -0300

    feat: remove latest avatar if update user avatar

--- src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -1,4 +1,5 @@
 import { inject, injectable } from "tsyringe";
+import { deleteFile } from "../../../../utils/file";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 interface IResquest {
@@ -13,6 +14,8 @@ class UpdateUserAvatarUseCase {
   ) {}
   async execute({ user_id, avatar_file }: IResquest): Promise<void> {
     const user = await this.usersRepository.findById(user_id);
+
+    if (user.avatar) await deleteFile(`./tbm/avatar/${user.avatar}`);
 
     user.avatar = avatar_file;
 
