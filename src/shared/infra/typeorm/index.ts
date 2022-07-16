a787f655857f9076b58ed206a07be6996bde51d0
Italo Gabriel <italog03@gmail.com>
2022-07-16 19:57:18 -0300

    feat: add CreateCars migratino

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -2,13 +2,13 @@ import { DataSource } from 'typeorm'
 
 const dataSource = new DataSource({
     type: 'postgres',
-    host: 'database_ignite', // change localhost to run migration
+    host: 'localhost', // change localhost to run migration
     port: 5432,
     username: "docker",
     password: "ignite",
     database: "rentx",
     entities: ["src/modules/**/entities/*.ts"],
-    migrations: ["src/database/migrations/*.ts"]
+    migrations: ["src/shared/infra/typeorm/migrations/*.ts"]
 })
 
 dataSource.initialize()
