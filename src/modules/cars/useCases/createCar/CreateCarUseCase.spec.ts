11e58ea6bed1931bc9236e1ffab3268976466faa
Italo Gabriel <italo.pereira@pagtel.com.br>
2022-07-18 10:12:47 -0300

    feat: generate car with avaliable true

--- src/modules/cars/useCases/createCar/CreateCarUseCase.spec.ts
+++ src/modules/cars/useCases/createCar/CreateCarUseCase.spec.ts
@@ -9,9 +9,7 @@ describe("Create Car", () => {
 
   beforeEach(() => {
     carsRepositoryInMemory = new CarsRepositoryInMemory();
-    createCarUseCase = new CreateCarUseCase(
-      carsRepositoryInMemory
-    );
+    createCarUseCase = new CreateCarUseCase(carsRepositoryInMemory);
   });
 
   it("should be able to create a new car", async () => {
@@ -22,7 +20,45 @@ describe("Create Car", () => {
       license_plate: "ABC-1234",
       fine_amount: 60,
       brand: "Brans",
-      category_id: "category"
-    })
+      category_id: "category",
+    });
+  });
+
+  it("should not be able to create a car with exists license plate", async () => {
+    expect(async () => {
+      await createCarUseCase.execute({
+        name: "Name Car",
+        description: "Description Car",
+        daily_rate: 100,
+        license_plate: "ABC-1234",
+        fine_amount: 60,
+        brand: "Brans",
+        category_id: "category",
+      });
+
+      await createCarUseCase.execute({
+        name: "Name Car2",
+        description: "Description Car",
+        daily_rate: 100,
+        license_plate: "ABC-1234",
+        fine_amount: 60,
+        brand: "Brans",
+        category_id: "category",
+      });
+    });
+  });
+
+  it("should be able to create a car with available true by default", async () => {
+    const car = await createCarUseCase.execute({
+      name: "Name Car",
+      description: "Description Car",
+      daily_rate: 100,
+      license_plate: "ABC-1234",
+      fine_amount: 60,
+      brand: "Brans",
+      category_id: "category",
+    });
+
+    expect(car.available).toBe(true);
   });
 });
