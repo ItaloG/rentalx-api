0d97d245fc06ac453b89f36a92e9de09dbcf0279
Italo Gabriel <italo.pereira@pagtel.com.br>
2022-07-18 09:57:58 -0300

    feat: add license plate validation

--- src/modules/cars/useCases/createCar/CreateCarUseCase.ts
+++ src/modules/cars/useCases/createCar/CreateCarUseCase.ts
@@ -1,4 +1,5 @@
 import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
+import { AppError } from "@shared/errors/AppError";
 import { inject, injectable } from "tsyringe";
 
 interface IRequest {
@@ -25,7 +26,13 @@ class CreateCarUseCase {
     brand,
     category_id,
   }: IRequest): Promise<void> {
-    this.carsRepository.create({
+    const carAlreadyExists = await this.carsRepository.findByLicensePlate(
+      license_plate
+    )
+
+    if(carAlreadyExists) throw new AppError("Car already exists!")
+
+    await this.carsRepository.create({
       name,
       description,
       daily_rate,
