5eaf801c037f58cc76d1b4a50074ce8ae7067bcb
Italo Gabriel <italo.pereira@pagtel.com.br>
2022-07-19 10:31:10 -0300

    add specifications car route

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -1,20 +1,30 @@
 import { CreateCarController } from "@modules/cars/useCases/createCar/CreateCarController";
+import { CreateCarSpecificationController } from "@modules/cars/useCases/createCarSpecification/CreateCarSpecificationController";
 import { ListAvailableCarsController } from "@modules/cars/useCases/listAvailableCars/ListAvailableCarsController";
 import { Router } from "express";
+import { ensureAdmin } from "../middlewares/ensureAdmin";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
 const carsRoutes = Router();
 
 const createCarController = new CreateCarController();
 const listAvailableCarsController = new ListAvailableCarsController();
+const createCarSpecificationController = new CreateCarSpecificationController();
 
 carsRoutes.post(
   "/",
   ensureAuthenticated,
-  ensureAuthenticated,
+  ensureAdmin,
   createCarController.handle
 );
 
-carsRoutes.get("/", ensureAuthenticated, listAvailableCarsController.handle);
+carsRoutes.get("/available", listAvailableCarsController.handle);
+
+carsRoutes.post(
+  "/specifications/:id",
+  ensureAuthenticated,
+  ensureAdmin,
+  createCarSpecificationController.handle
+);
 
 export { carsRoutes };
