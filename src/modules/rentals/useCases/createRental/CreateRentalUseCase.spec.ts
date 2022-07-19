333be112b740ae9ff37d68b32f5f57f6c85a10f8
Italo Gabriel <italo.pereira@pagtel.com.br>
2022-07-19 15:23:03 -0300

    feat: create rental

---
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.spec.ts
@@ -0,0 +1,23 @@
+import { RentalsRepositoryInMemory } from "@modules/rentals/repositories/in-memory/RentalsRepositoryInMemory";
+import { CreateRentalUseCase } from "./CreateRentalUseCase";
+
+let createRentalUseCase: CreateRentalUseCase;
+let rentalsRepositoryInMemory: RentalsRepositoryInMemory;
+
+describe("Create Rental", () => {
+  beforeEach(() => {
+    rentalsRepositoryInMemory = new RentalsRepositoryInMemory();
+    createRentalUseCase = new CreateRentalUseCase(rentalsRepositoryInMemory);
+  });
+
+  it("should be able to create a new rental", async () => {
+    const rental = await createRentalUseCase.execute({
+        car_id: "211212",
+        user_id: "211212",
+        expected_return_date: new Date()
+    });
+
+    expect(rental).toHaveProperty("id");
+    expect(rental).toHaveProperty("start_date");
+  });
+});
