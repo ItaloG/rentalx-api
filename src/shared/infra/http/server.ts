4d00f851b0a5fabb6c26b14ec8c0738f68aa67df
Italo Gabriel <italog03@gmail.com>
2022-09-22 22:00:04 -0300

    feat: add cors

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -9,6 +9,7 @@ import "@shared/container";
 import { router } from "./routes";
 import { AppError } from "@shared/errors/AppError";
 import upload from "@config/upload";
+import cors from 'cors'
 
 const app = express();
 
@@ -19,6 +20,7 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
 app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
 
+app.use(cors())
 app.use(router);
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
