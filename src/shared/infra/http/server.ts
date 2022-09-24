4ea4560d92aa11bf7262645ed2c006880d14f8ec
Italo Gabriel <italog03@gmail.com>
2022-09-24 20:53:30 -0300

    feat: add sentry logs

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -11,10 +11,25 @@ import { AppError } from "@shared/errors/AppError";
 import upload from "@config/upload";
 import cors from "cors";
 import rateLimiter from "./middlewares/rateLimiter";
+import * as Sentry from "@sentry/node";
+import * as Tracing from "@sentry/tracing";
 
 const app = express();
 
 app.use(rateLimiter);
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [
+    new Sentry.Integrations.Http({ tracing: true }),
+    new Tracing.Integrations.Express({ app }),
+  ],
+
+  tracesSampleRate: 1.0,
+});
+
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
 
 app.use(express.json());
 
@@ -25,6 +40,8 @@ app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
 
 app.use(cors());
 app.use(router);
+
+app.use(Sentry.Handlers.errorHandler());
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   if (err instanceof AppError) {
